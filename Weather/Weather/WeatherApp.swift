a83e177405c1342883b5aeca2a1199294756a358
Dyobi <dyobi.92@gmail.com>
2022-04-08 23:56:01 +0900

    .

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Dyobi on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
