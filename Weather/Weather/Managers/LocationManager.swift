44419eea1345f4da6a9593ca41fc889b5077d643
Dyobi <dyobi.92@gmail.com>
2022-04-08 23:48:55 +0900

    .

---
+++ Weather/Weather/Managers/LocationManager.swift
@@ -0,0 +1,38 @@
+//
+//  LocationManager.swift
+//  Weather
+//
+//  Created by Dyobi on 4/7/22.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    let manager = CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isLoading: Bool = false
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation() {
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error getting location", error)
+        isLoading = false
+    }
+    
+}
